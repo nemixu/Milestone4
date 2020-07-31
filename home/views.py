22033139bc7d929f051279ccdf77dc31ac2edc23
nemixu <s.r.seagrave@gmail.com>
2020-07-31 22:59:49 +0100

    Added view for about page to return a render template

--- home/views.py
+++ home/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def index(request):
     """ View to return the index page """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """Returns the about page"""
+    return render(request, 'home/about.html')
