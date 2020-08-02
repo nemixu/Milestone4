8ff114c39fa98c315299c6cbecca4a34bea652b4
nemixu <s.r.seagrave@gmail.com>
2020-08-02 20:25:06 +0100

    created context to pass to all views, loop through items

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def cart_contents(request):
@@ -7,12 +8,27 @@ def cart_contents(request):
      cart_items = []
      total = 0
      product_count = 0
+     delivery = 0
+     cart = request.session.get('cart', {})
      
+     for item_id, quantity in cart.items():
+         product = get_object_or_404(Product, pk=item_id)
+         total += quantity * product.price
+         product_count += quantity 
+         cart_items.append({
+             'item_id': item_id,
+             'quantity': quantity,
+             'product' : product,
+         })
+     
+     grand_total = total
      
      context = {
          'cart_items' : cart_items,
          'total' : total,
          'product_count' : product_count,
+         'delivery' : delivery,
+         'grand_total' : grand_total,
      }
      
      return context
