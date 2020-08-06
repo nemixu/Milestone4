5da17038054cc479be6d3066ffd28aff26695575
nemixu <s.r.seagrave@gmail.com>
2020-08-06 22:46:07 +0100

    added views for updating and removing cart details and urls for each

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
+    path('update/<item_id>/', views.update_cart, name='update_cart'),
+    path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, reverse, redirect, HttpResponse
 
 # Create your views here.
 
@@ -9,7 +9,6 @@ def cart(request):
 
 def add_to_cart(request, item_id):
     """Add an item or quantitity of items to the cart"""
-    # product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))    
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -21,3 +20,28 @@ def add_to_cart(request, item_id):
         
     request.session['cart'] = cart   
     return redirect(redirect_url)
+
+
+def update_cart(request, item_id):
+    """update the cart quantity"""
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+        
+    request.session['cart'] = cart
+    return redirect(reverse('cart'))
+
+
+def remove_from_cart(request, item_id):
+    """remove an item from the cart"""
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
