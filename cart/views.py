f9454d31f6d56f960e81dfef4acc773a273af218
nemixu <s.r.seagrave@gmail.com>
2020-08-07 19:26:26 +0100

    Imported messages and products and included messages in the views

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, reverse, redirect, HttpResponse
+from django.shortcuts import render, reverse, redirect, HttpResponse, get_object_or_404
+from django.contrib import messages
 
+from products.models import Product
 # Create your views here.
 
 def cart(request):
@@ -9,14 +11,17 @@ def cart(request):
 
 def add_to_cart(request, item_id):
     """Add an item or quantitity of items to the cart"""
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))    
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
     
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.info(request, f'{ product.name } is already in the cart, edit the quantity for more of this product')
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added { product.name } to your cart')
         
     request.session['cart'] = cart   
     return redirect(redirect_url)
@@ -26,9 +31,10 @@ def update_cart(request, item_id):
     """update the cart quantity"""
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
-    
+    product = get_object_or_404(Product, pk=item_id)
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated { product.name } quantity in your cart')
     else:
         cart.pop(item_id)
         
@@ -39,9 +45,11 @@ def update_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """remove an item from the cart"""
     try:
+        product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
         request.session['cart'] = cart
+        messages.success(request, f'Removed { product.name } from your cart')
         return HttpResponse(status=200)
     except Exception as e:
         return HttpResponse(status=500)
