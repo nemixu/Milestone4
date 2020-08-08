7777a4814d2acc97720969fae23a6af7480ca2d3
nemixu <s.r.seagrave@gmail.com>
2020-08-08 18:59:19 +0100

    Created admin classes for the admin section to view orders and update accordingly

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Order,  OrderLineItem
 
-# Register your models here.
+class OrderLineItemAdminInLine(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+    
+    
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInLine,)
+    
+    readonly_fields = ('order_number', 'order_date', 
+                       'delivery_cost', 'order_total', 'grand_total',)
+    
+    fields = ('order_number', 'order_date', 'full_name', 'email',
+              'phone_number', 'delivery_cost', 'order_total', 
+              'grand_total')
+
+
+    list_display = ('order_number','order_date', 'full_name',
+                    'order_total', 'delivery_cost', 'grand_total',)
+    
+    ordering = ('-order_date',)
+    
+admin.site.register(Order, OrderAdmin)    
