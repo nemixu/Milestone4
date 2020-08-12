7d8cfb50dd8b6b6af3a69aac20feaafc5c73caa4
nemixu <s.r.seagrave@gmail.com>
2020-08-12 14:22:01 +0100

    Added js for stipe elements from the stripe documentation

---
+++ checkout/static/js/stripe_elements.js
@@ -0,0 +1,29 @@
+const stripePublicKey = document.querySelector('#stripe-public-key').innerText.slice(1, -1);
+const clientSecret = document.querySelector('#client-secret').innerText.slice(1, -1);
+
+const stripe = Stripe(stripePublicKey);
+const elements = stripe.elements();
+
+
+const style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Roboto", sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        fontWeight: 'normal',
+        '::placeholder': {
+            color: '#aab7c4'
+        },
+        backgroundColor: 'white',
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+
+const card = elements.create('card', {style: style});
+
+card.mount('#card-element');
+
