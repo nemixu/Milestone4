dba297320d28f7d2f875c413788809cdcca01d50
nemixu <s.r.seagrave@gmail.com>
2020-08-13 14:38:56 +0100

    added payment intent and variables for stripe, imported stripe

--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,8 @@ from .models import OrderLineItem, Order
 from products.models import Product
 from cart.contexts import cart_contents
 
+import stripe 
+
 
 
 def checkout(request):
@@ -19,12 +21,25 @@ def checkout(request):
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment!")
         return redirect(reverse('products'))
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
     
     order_form = OrderForm()    
+    
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing')
+    
     template = 'checkout/checkout.html'
     context = {
         'order_form' : order_form,
         'stripe_public_key': stripe_public_key,
+        'client_secret' : intent.client_secret,
     }    
     
     return render(request, template, context)    
