505a5f242af71d81cbb17c31a7b0b5a15c32f68a
nemixu <s.r.seagrave@gmail.com>
2020-08-13 14:37:47 +0100

    Added the basic checkout functionality handling errors

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -4,7 +4,6 @@ const clientSecret = document.querySelector('#client-secret').innerText.slice(1,
 const stripe = Stripe(stripePublicKey);
 const elements = stripe.elements();
 
-
 const style = {
     base: {
         color: '#000',
@@ -24,6 +23,50 @@ const style = {
 };
 
 const card = elements.create('card', {style: style});
-
 card.mount('#card-element');
 
+// Handle realtime validation errors on the card element
+card.addEventListener('change', e => {
+    const errorDiv = document.getElementById('card-errors');
+    if (e.error) {
+        let html  = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${e.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+const form = document.getElementById('payment-form');
+
+form.addEventListener('submit', ev => {
+    ev.preventDefault();
+    card.update({ 'disabled': true });
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(result => {
+        if (result.error) {
+            const errorDiv = document.getElementById('card-errors');
+            let html  = `
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false });
+            $('#submit-button').attr('disabled', false);
+
+        } else {
+          if (result.paymentIntent.status === 'succeeded') {
+              form.submit();
+          }  
+        }
+    });
+});
