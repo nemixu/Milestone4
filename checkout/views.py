8236a9918871fd141829755de86ff8b26898c43a
nemixu <s.r.seagrave@gmail.com>
2020-08-15 14:49:09 +0100

    created view to handle cache checkout data

--- checkout/views.py
+++ checkout/views.py
@@ -9,7 +9,27 @@ from products.models import Product
 from cart.contexts import cart_contents
 
 import stripe 
+import json
 
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
+        
+        
+        
+        
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLISHABLE
     stripe_secret_key = settings.STRIPE_SECRET
