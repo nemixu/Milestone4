1203702e45309a885226a01f0552e4df063b89bc
nemixu <s.r.seagrave@gmail.com>
2020-08-15 23:26:42 +0100

    added user_name to the models in checkout and imported profiles

--- checkout/models.py
+++ checkout/models.py
@@ -5,11 +5,13 @@ from django.db.models import Sum
 from django.conf import settings
 
 from products.models import Product
+from profiles.models import Profile
 # Create your models here.
 
 class Order(models.Model):
     """Model to define the fields required to create an order in the database"""
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_name = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=40, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
