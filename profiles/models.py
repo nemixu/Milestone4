72b7d3a210acb5a78b7ccd8e4cc4f7f85a1ea050
nemixu <s.r.seagrave@gmail.com>
2020-08-15 23:28:01 +0100

    created a user profile class which returns the username &create or update receiver

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    """A user profile to maintain user information and store order history"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """Create or update the user profile"""
+    if created:
+        Profile.objects.create(user=instance)
+    # If user is existing - save the profile change
+    instance.userprofile.save()    
