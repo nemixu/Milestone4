190badb66654d7b5f9a2d179e995a3f87b865655
nemixu <s.r.seagrave@gmail.com>
2020-08-17 23:48:13 +0100

    models for profile created, returning username save user profile if exists, create otherwise

--- profiles/models.py
+++ profiles/models.py
@@ -5,9 +5,12 @@ from django.dispatch import receiver
 
 class UserProfile(models.Model):
     """A user profile to maintain user information and store order history"""
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)   
+    default_full_name = models.CharField(max_length=40, null=True, blank=True)
+    default_email = models.EmailField(max_length=254, null=True, blank=True)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     
+
     def __str__(self):
         return self.user.username
     
