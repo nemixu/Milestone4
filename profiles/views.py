afc0642ddd41d2dd6c75c9f282838d2ac5672184
nemixu <s.r.seagrave@gmail.com>
2020-08-17 23:48:49 +0100

    created  view to return the profile and form

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render,  get_object_or_404
 
 from .models import UserProfile
+from.forms import UserProfileForm
+
 # Create your views here.
 def profile(request):
     """View to display the users profile"""
     profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+    
+    form = UserProfileForm(instance=profile)
     template ='profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
     return render(request, template, context)
