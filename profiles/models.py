059990873c28f5b0572070ba217a5bf65be5fe47
nemixu <s.r.seagrave@gmail.com>
2020-08-17 20:48:58 +0100

    updated models name for userprofile

--- profiles/models.py
+++ profiles/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class Profile(models.Model):
+class UserProfile(models.Model):
     """A user profile to maintain user information and store order history"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
@@ -15,6 +15,6 @@ class Profile(models.Model):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """Create or update the user profile"""
     if created:
-        Profile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance)
     # If user is existing - save the profile change
     instance.userprofile.save()    
