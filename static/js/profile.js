6f2d07ce425e876b35ea84fdcb49ded7b20af65f
nemixu <s.r.seagrave@gmail.com>
2020-08-19 23:26:16 +0100

    added click function to handle active class on profile nav

--- static/js/profile.js
+++ static/js/profile.js
@@ -2,6 +2,13 @@ const profilePanel = $('#account-profile-panel');
 const ordersPanel = $('#account-orders-panel');
 const logOutPanel = $('#account-profile-logout-panel');
 
+//click function to handle the active class for profile navigation
+$('li').click(function() {
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+});
+
+//onclick events to handle which panel is showing per click | default is My account /contact information
 $('#account-profile').click(function(){
     profilePanel.show();
     ordersPanel.hide();
