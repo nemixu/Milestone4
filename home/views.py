eab39543ccc677b3785c4f760aca076b48f6d395
nemixu <s.r.seagrave@gmail.com>
2020-08-20 20:18:11 +0100

    created a context to pass  the page title and other needed variables

--- home/views.py
+++ home/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+
 
 # Create your views here.
 
 def index(request):
     """ View to return the index page """
     page_title = "Home Page"
-    return render(request, 'home/index.html', {'page_title': page_title})
+    template = "home/index.html"
+    context = {
+        'page_title': page_title,
+    }
+    return render(request, template, context)
 
 
 def about(request):
     """Returns the about page"""
     page_title = "About us"
-    return render(request, 'home/about.html', {'page_title': page_title})
+    template = 'home/about.html'
+    context = {
+        'page_title': page_title,
+    }
+    return render(request, template, context)
+
