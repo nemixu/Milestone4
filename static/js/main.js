836bae2c8c7f6eaa82dd140b536b3356da634c27
nemixu <s.r.seagrave@gmail.com>
2020-08-22 21:36:10 +0100

    Added onclick handlers for popup, when admin removes product

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,20 @@
-console.log('test to ensure file path is working')
-
+//Boostrap toasts
 $('.toast').toast('show');
+
+//Delete confirmation poopup for admin removing products
+const deleteButton = $('#delete-button');
+const deletePopup = $('.overlay-delete');
+const popupText = $('#delete-popup');  
+
+deleteButton.click(function(){
+    deletePopup.show();
+    deletePopup.addClass('overlay-show');
+    popupText.show();
+    return false;
+});
+
+deletePopup.click(function(){
+    deletePopup.hide();
+    deletePopup.removeClass('overlay-show');
+    popupText.hide();
+});
