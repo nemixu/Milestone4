4db1cb85b80f4f72ba266553bad9a59a6952d2e4
nemixu <s.r.seagrave@gmail.com>
2020-08-23 13:54:20 +0100

    added review model class in the products model

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     class Meta:
@@ -26,3 +27,14 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+    
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True, related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="reviews")
+    comment = models.TextField(max_length=1000, blank=True, null=True)
+    rating = models.FloatField(default=0)
+    
+    def __str__(self):
+        return self.user.username
+    
