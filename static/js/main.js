0823392c6b2244875d0a4e1f018702210e14d89b
nemixu <s.r.seagrave@gmail.com>
2020-08-27 19:44:29 +0100

    Added onclick handler for mobile side nav bar

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,17 @@
 //Boostrap toasts
 $('.toast').toast('show');
 
-//Delete confirmation poopup for admin removing products
+//Product management Delete confirmation poopup for admin removing products
 const deleteButton = $('#delete-button');
 const deletePopup = $('.overlay-delete');
 const popupText = $('#delete-popup');  
 
+// Mobile Side nav
+const burgerIcon = $('.burger-button');
+const mobileNav = $('.mobile-nav');
+const mobileNavBg = $('.nav-background');
+
+// Product management on click handler
 deleteButton.click(function(){
     deletePopup.show();
     deletePopup.addClass('overlay-show');
@@ -19,9 +25,17 @@ deletePopup.click(function(){
     popupText.hide();
 });
 
-/* Nav Icon */
-$(document).ready(function(){
-	$('#burger-icon').click(function(){
-		$(this).toggleClass('open');
-	});
+
+// Mobile Side nav handler
+
+burgerIcon.click(function(){
+    mobileNavBg.show();
+    mobileNavBg.addClass('overlay-nav');
+    mobileNav.addClass('transition');
+    return false;
 });
+
+mobileNavBg.click(function(){
+    mobileNavBg.removeClass('overlay-nav');
+    mobileNav.removeClass('transition');
+})
