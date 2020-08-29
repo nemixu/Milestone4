e507ef7466c4c8bd59e01a3485f236b4254d28af
nemixu <s.r.seagrave@gmail.com>
2020-08-29 20:13:14 +0100

    Added docstrings, removed white spacing

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,26 +1,32 @@
 from django.contrib import admin
-from .models import Order,  OrderLineItem
+from .models import Order, OrderLineItem
 
 class OrderLineItemAdminInLine(admin.TabularInline):
+    """
+    Order lineItem class to display tabularinline subclass
+    """
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
-    
-    
+
+
 class OrderAdmin(admin.ModelAdmin):
+    """
+    Order admin class to order the fields and view of the Order model
+    """
     inlines = (OrderLineItemAdminInLine,)
-    
-    readonly_fields = ('order_number', 'order_date', 
+
+    readonly_fields = ('order_number', 'order_date',
                        'delivery_cost', 'order_total', 'grand_total',
                        'original_cart', 'stripe_pid')
-    
+
     fields = ('order_number', 'user_profile', 'order_date', 'full_name', 'email',
-              'phone_number', 'delivery_cost', 'order_total', 
+              'phone_number', 'delivery_cost', 'order_total',
               'grand_total', 'original_cart', 'stripe_pid')
 
 
-    list_display = ('order_number','order_date', 'full_name',
+    list_display = ('order_number', 'order_date', 'full_name',
                     'order_total', 'delivery_cost', 'grand_total',)
-    
+
     ordering = ('-order_date',)
-    
-admin.site.register(Order, OrderAdmin)    
+
+admin.site.register(Order, OrderAdmin)
