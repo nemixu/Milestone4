a550411c378ed762414a82e10df98867c715ce34
nemixu <s.r.seagrave@gmail.com>
2020-08-29 20:17:24 +0100

    created contact view to instantiateform, handle post of form, created success view

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.core.mail import send_mail
+
+from .forms import Contact
 
 
 # Create your views here.
@@ -27,24 +29,37 @@ def about(request):
 def contact(request):
     """View to handle the contact page"""
     page_title = "Contact Us"
-    
     if request.method == "POST":
-        message_name = request.POST['message-name']
-        message_email = request.POST['message-email']
-        message = request.POST['message']
-        
-        send_mail(
-            'Contact from website' + message_name,
-            message,
-            message_email,
-            ['test@test.com'],
-        )
-    
-    
+        form = Contact(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for reaching out, we will be in touch shortly')
+            return redirect(reverse('contact_success'))
+            # send_mail(
+            #     'Contact from website' + message_name,
+            #     message,
+            #     message_email,
+            #     ['test@test.com'],
+            # )
+    else:
+        form = Contact()
+
     template = 'home/contact.html'
     context = {
         'page_title': page_title,
         'send_mail' : send_mail,
+        'form':form,
+    }
+
+    return render(request, template, context)
+
+def contact_success(request):
+    page_title = "Thanks for contacting"
+    template = "home/contact_success.html"
+    
+    context = {
+        'page_title' : page_title
     }
     
     return render(request, template, context)
