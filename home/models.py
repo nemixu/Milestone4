a7e72b003fdcbd005115b7cadc2086ef5839379a
nemixu <s.r.seagrave@gmail.com>
2020-08-29 20:15:16 +0100

    Created contact form model with fields required for the db and form

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class ContactForm(models.Model):
+    """
+    Model to define the fields required to create a contact form request in the database
+    """
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone = models.CharField(max_length=20, null=False, blank=False, default='')
+    message = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.message}'
