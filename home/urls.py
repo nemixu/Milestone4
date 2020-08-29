94846c60fa9a074758044aa6508b92cdb5395fd0
nemixu <s.r.seagrave@gmail.com>
2020-08-29 20:16:06 +0100

    created url for a contact success page

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about'),
-    path('contact', views.contact, name='contact'),
+    path('contact/', views.contact, name='contact'),
+    path('contact/success/', views.contact_success, name='contact_success'),
 ]
