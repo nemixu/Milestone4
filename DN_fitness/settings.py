69df0fd47c35d9e1684acacd0b54f616ae305001
nemixu <s.r.seagrave@gmail.com>
2020-08-30 17:19:58 +0100

    added admin email to be contact when contact is requested

--- DN_fitness/settings.py
+++ DN_fitness/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'checkout',
     'search',
     'profiles',
-    
+
     # Other
     'crispy_forms',
 ]
@@ -183,6 +183,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = os.environ.get('DEVELOPMENT_FROM_EMAIL')
+    ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
@@ -191,4 +192,5 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
     
