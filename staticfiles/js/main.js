175c02fe812f31e5610f4ebd6d8715dfbb73238d
nemixu <s.r.seagrave@gmail.com>
2020-08-31 15:17:35 +0100

    added static files dir

---
+++ staticfiles/js/main.js
@@ -0,0 +1,46 @@
+//Boostrap toasts
+$('.toast').toast('show');
+
+//Product management Delete confirmation poopup for admin removing products
+const deleteButton = $('#delete-button');
+const deletePopup = $('.overlay-delete');
+const popupText = $('#delete-popup');  
+
+// Mobile Side nav
+const burgerIcon = $('.burger-button');
+const mobileNav = $('.mobile-nav');
+const mobileNavBg = $('.nav-background');
+
+// Product management on click handler
+deleteButton.click(function(){
+    deletePopup.show();
+    deletePopup.addClass('overlay-show');
+    popupText.show();
+    return false;
+});
+
+deletePopup.click(function(){
+    deletePopup.hide();
+    deletePopup.removeClass('overlay-show');
+    popupText.hide();
+});
+
+
+// Mobile Side nav handler
+
+burgerIcon.click(function(){
+    mobileNavBg.show();
+    mobileNavBg.addClass('overlay-nav');
+    mobileNav.addClass('transition');
+    return false;
+});
+
+mobileNavBg.click(function(){
+    mobileNavBg.removeClass('overlay-nav');
+    mobileNav.removeClass('transition');
+})
+
+// To top scroll
+$('.totop-link').click(function(e){
+    window.scrollTo(0,0)
+})
---
+++ staticfiles/js/stripe_elements.js
@@ -0,0 +1,97 @@
+const stripePublicKey = document.querySelector('#stripe-public-key').innerText.slice(1, -1);
+const clientSecret = document.querySelector('#client-secret').innerText.slice(1, -1);
+
+const stripe = Stripe(stripePublicKey);
+const elements = stripe.elements();
+
+const style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Roboto", sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '1.2rem',
+        fontWeight: 'normal',
+        '::placeholder': {
+            color: '#aab7c4'
+        },
+        backgroundColor: '#ffffff',
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+
+const card = elements.create('card', {style: style});
+card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', e => {
+    const errorDiv = document.getElementById('card-errors');
+    if (e.error) {
+        let html  = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${e.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+const form = document.querySelector('#payment-form');
+const submitButton = document.querySelector('#submit-button');
+
+form.addEventListener('submit', ev => {
+    ev.preventDefault();
+    card.update({ 'disabled': true });
+    submitButton.setAttribute('disabled', true);
+    $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#spinner-overlay').fadeToggle(100);
+
+    const saveInfo = Boolean($('#id-save-info').attr('checked'));
+    const csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    const postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    const url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function() {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                }
+            },
+        }).then(result => {
+            if (result.error) {
+                const errorDiv = document.getElementById('card-errors');
+                let html  = `
+                    <span class="icon" role="alert">
+                        <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#spinner-overlay').fadeToggle(100);
+                card.update({ 'disabled': false });
+                $('#submit-button').attr('disabled', false);
+    
+            } else {
+              if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+              }  
+            }
+        });
+    }).fail(function(){
+        location.reload();
+    })   
+});
