2d5d67c5f408a518f8f6ca1883b1582b76f7ef0c
nemixu <s.r.seagrave@gmail.com>
2020-08-31 15:18:03 +0100

    adjusted debug

--- DN_fitness/settings.py
+++ DN_fitness/settings.py
@@ -125,7 +125,7 @@ ACCOUNT_LOGOUT_ON_GET = True
 if "DATABASE_URL" in os.environ:
     DATABASES = {'default': dj_database_url.parse(
         os.environ.get('DATABASE_URL'))}
-else:    
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
